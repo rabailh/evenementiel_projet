f43608292d9e74bc54e516c2378717459b8c9ed1
rbailhet <raphael.bailhet@etu.u-bordeaux.fr>
2022-11-23 22:45:01 +0100

    1 - init projet java

---
+++ evenementiel/src/main/java/bailhet/boisson/evenementiel/Evenementiel.java
@@ -0,0 +1,12 @@
+package bailhet.boisson.evenementiel;
+
+/**
+ *
+ * @author Raphael BAILHET & Manon BOISSON
+ */
+public class Evenementiel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
