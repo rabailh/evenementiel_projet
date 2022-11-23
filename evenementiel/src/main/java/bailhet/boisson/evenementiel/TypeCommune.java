d79519b42d4ff86cb7097c5157ff5daefef820c8
rbailhet <raphael.bailhet@etu.u-bordeaux.fr>
2022-11-23 22:48:39 +0100

    3 - Enums Types evenement & commune

---
+++ evenementiel/src/main/java/bailhet/boisson/evenementiel/TypeCommune.java
@@ -0,0 +1,9 @@
+package bailhet.boisson.evenementiel;
+
+/**
+ *
+ * @author Raphael BAILHET & Manon BOISSON
+ */
+public enum TypeCommune {
+    VILLAGE, VILLE, MEGALOPOLE;
+}
---
+++ evenementiel/src/main/java/bailhet/boisson/evenementiel/TypeEvenement.java
@@ -0,0 +1,9 @@
+package bailhet.boisson.evenementiel;
+
+/**
+ *
+ * @author Raphael BAILHET & Manon BOISSON
+ */
+public enum TypeEvenement {
+    FETE_DE_VILLAGE, CONCERT, JEUX_OLYMPIQUES;
+}
