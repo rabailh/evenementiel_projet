92762d3bf26ecfe06caab75c35e2627aa60867c2
mannboisson <manon.boisson@icloud.com>
2022-11-26 17:06:15 +0100

    6 - Implem classe Village  + TU

--- evenementiel/src/main/java/bailhet/boisson/evenementiel/Village.java
+++ evenementiel/src/main/java/bailhet/boisson/evenementiel/Village.java
@@ -9,15 +9,22 @@ public class Village extends Commune{
     public Village(String nom) {
         super(nom);
     }
+    //Todo ajouter maire
     
     @Override
     public Boolean VerifierPossibilitéEvenement(TypeEvenement evenement){
-        return true;
+        return (TypeCommune.VILLAGE.nbHabitants > evenement.nbPersonneEvenement);
+
     }
     
     @Override
     public void OrganiserUnEvenement(TypeEvenement evenement){
-        
+        if(VerifierPossibilitéEvenement(evenement) == true) {
+          //Evenement evt = new Evenement();
+        } else {
+            System.out.println("Vous ne pouvez pas organiser un " + evenement + ". ");
+        }
     }
+    // Todo Tu
     
 }
